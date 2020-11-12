c2ffd2214139ca4217fbc70b21f6a5af2c39f784
nostam <matsonho@gmail.com>
2020-11-12 14:42:37 +0100

    init M3-D9

--- js/edit.js
+++ js/edit.js
@@ -3,28 +3,37 @@ const myHeaders = new Headers({
     "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZmFiZDRiMjRiY2RlMTAwMTc2MTZhOWQiLCJpYXQiOjE2MDUwOTY2MjYsImV4cCI6MTYwNjMwNjIyNn0.wnvHZoGWTVKKmnMosaLbRybRZBimbBtCTwikNc7HA_0",
   "Content-Type": "application/json",
 });
+
 const url = "https://striveschool-api.herokuapp.com/api/product/";
 
 const handleSubmit = (e) => {
   e.preventDefault(); // preventing the default browser event handling
   submitProduct();
 };
-const isNumber = (n) => {
+
+const isPositiveNumber = (n) => {
   return !isNaN(parseFloat(n)) && isFinite(n) && n > 0;
 };
+
 const submitProduct = async () => {
+  let spinner = document.querySelector("#loadingSpinner");
+  spinner.classList.toggle("d-none"); // showing the spinner
+  document.getElementById("submitBtn").innerText = "Submitting...";
+
+  // input validation
   let inputs = [...document.querySelectorAll("input")];
-  inputs.flat(inputs.push([...document.querySelectorAll("textarea")]));
-  if (isNumber(inputs[3])) {
+  // inputs.flat(inputs.push([...document.querySelectorAll("textarea")]));
+  if (isPositiveNumber(inputs[3])) {
     console.log("price is correct");
   } else {
     throw Error("incorrect price");
   }
-  if (Boolean(inputs.filter((e) => e.value === "").length === 0)) {
-    console.log("data all set");
-  } else {
-    throw Error("empty field exists");
-  }
+  // if (Boolean(inputs.filter((e) => e.value === "").length === 0)) {
+  //   console.log("data all set");
+  // } else {
+  //   throw Error("empty field exists");
+  // } //replaced with required tag
+
   let newProduct = {
     name: document.querySelector("#name").value,
     description: document.querySelector("#description").value,
@@ -34,7 +43,9 @@ const submitProduct = async () => {
   };
 
   try {
-    let response = await fetch(url, {
+    let response;
+
+    response = await fetch(url, {
       // our POST request is made with the fetch method as well!
       method: "POST", // declaring the CRUD method
       body: JSON.stringify(newProduct), // we need to stringify the JS object in order to send it
@@ -42,9 +53,13 @@ const submitProduct = async () => {
     });
 
     if (response.ok) {
+      spinner.classList.toggle("d-none");
+      document.getElementById("submitBtn").innerText = "Success!";
       alert("Product created successfully, redirecting back to homepage...");
       location.assign("index.html");
     } else {
+      spinner.classList.toggle("d-none");
+      document.getElementById("submitBtn").innerText = "Submit Product";
       alert("Something went wrong!");
     }
   } catch (error) {
--- js/index.js
+++ js/index.js
@@ -8,16 +8,18 @@ const addToShelf = (p) => {
   let card = document.createElement("div");
   card.classList.add("card", "mx-auto", "my-5");
   card.style.width = "300px";
-  card.setAttribute("id", p._id);
   card.innerHTML = `
     <img src="${p.imageUrl}" class="card-img-top">
     <div class="card-body">
       <h5 class="card-title d-flex justify-content-between"><span>${p.name}</span><span>${p.brand}</span></h5>
       <p class="card-text">${p.description}</p>
     </div>
-    <div class="card-footer d-flex justify-content-between align-items-center">
-      <span>Price: $${p.price}</span>
-      <a href="#" class="btn btn-warning">Buy</a>
+    <div class="card-footer d-flex justify-content-between">
+      <a href="./detail.html?id=${p._id}" class="btn btn-info">Detail</a>
+      <div class="d-flex align-items-center">
+        <span>Price: $${p.price}</span>
+        <a href="#" class="btn btn-warning ml-2">Buy</a>
+      </div>
     </div>`;
   return card;
 };
@@ -82,9 +84,11 @@ const sortIndex = function (payload, option) {
 };
 
 window.onload = async () => {
+  let contentLoadingSpinner = document.getElementById("contentLoadingSpinner");
+  contentLoadingSpinner.classList.toggle("d-none");
   const url = "https://striveschool-api.herokuapp.com/api/product/";
   let shelf = document.querySelector("#shelf");
-  let sortBtn = document.querySelector(".btn-outline-secondary");
+  let sortBtn = document.querySelector("#sortBtn");
   try {
     let response = await fetch(url, {
       method: "GET",
@@ -93,11 +97,13 @@ window.onload = async () => {
 
     let payload = await response.json();
     if (payload.length > 0) {
+      contentLoadingSpinner.classList.toggle("d-none");
       document.getElementById("func").classList.toggle("d-none");
       sortBtn.addEventListener("click", sortIndex);
       payload.forEach((p) => shelf.appendChild(addToShelf(p)));
       search(payload);
     } else {
+      contentLoadingSpinner.classList.toggle("d-none");
       shelf.innerHTML = "<h2>Sorry it's out of stock at the moment</h2>";
     }
   } catch (error) {
